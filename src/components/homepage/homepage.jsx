146878a3ad09eed5ca5cadfc875d2913e3010ff6
ved <vedpratap.rajpoot@gmail.com>
2022-08-12 19:28:46 +0530

    modified authentication using jwt

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './homepage.css'
+import jwtDecode from 'jwt-decode';
 
-const Homepage = ({ user , setLoginUser}) => {
+const Homepage = ({ token , setToken}) => {
+
+  const user = jwtDecode(token);
+  console.log(user);
+
+  const logout = ()=>{
+    localStorage.removeItem('token');
+    setToken(null);
+    console.log(token);
+  }
   return (
     <div className='homepage'>
         <h1>Hello {user.name}</h1>
-        <div className="button" onClick={()=>{setLoginUser({})}}>Logout</div>
+        <div className="button" onClick={logout}>Logout</div>
     </div>
   )
 }
