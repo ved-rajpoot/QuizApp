6dd3f87e7096cf1cb79318f553f7cffa20b87049
ved <vedpratap.rajpoot@gmail.com>
2022-08-12 19:27:49 +0530

    Added new routes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 import Login from './components/login/login';
 import Register from './components/register/register';
 import Homepage from './components/homepage/homepage';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import { useState } from 'react';
+import { useEffect } from 'react';
+import jwtDecode from 'jwt-decode';
 
 function App() {
   const[ user, setLoginUser] = useState({
@@ -11,12 +13,29 @@ function App() {
       email:"",
       password:"" 
   })
+  const [token,setToken] = useState(null);
+  useEffect(()=>{
+    const token = localStorage.getItem('token');
+    if(token){
+      console.log(token);
+      const User = jwtDecode(token);
+      if(!User){
+        localStorage.removeItem('token');
+        setToken(null);
+      } else {
+        setToken(token);
+        console.log(User);
+      }
+      // setLoginUser(User);
+    }
+  },[]);
+
   return (
     <div className="App">
     <Router>
       <Routes>
-        <Route exact path="/" element={user && user._id? <Homepage user={user} setLoginUser={setLoginUser}/>: <Login setLoginUser={setLoginUser}/>}/>
-        <Route exact path="/login" element={<Login setLoginUser={setLoginUser}/>}/>
+        <Route exact path="/" element={ token ? <Homepage token={token} setToken={setToken}/>: <Login setToken={setToken}/>}/>
+        <Route exact path="/login" element={<Login setToken={setToken}/>}/>
         <Route exact path="/register" element={<Register/>}/>
       </Routes>
     </Router>
