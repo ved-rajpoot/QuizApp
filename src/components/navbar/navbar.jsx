c6d8c7e97d363b1cfbb91ea3536f84d1e63990b6
ved <vedpratap.rajpoot@gmail.com>
2022-08-15 08:09:02 +0530

    removed toggle bar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -16,11 +16,12 @@ const Navbar = ({user,setLoginUser}) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-    ">
         <Link className="navbar-brand" to="/">Navbar</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
-        </button>
+        </button> */}
         
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
+        <div className="" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
               <Link className="nav-link" to="/dashboard">Dashboard</Link>
