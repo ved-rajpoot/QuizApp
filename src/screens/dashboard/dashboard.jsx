0b0023f5cfa681a772eec255a590213fa4778352
ved <vedpratap.rajpoot@gmail.com>
2022-08-17 16:50:38 +0530

    converted a get request into post request in axios

--- src/screens/dashboard/dashboard.jsx
+++ src/screens/dashboard/dashboard.jsx
@@ -10,22 +10,14 @@ const Dashboard = ({user,setLoginUser}) => {
   const [attemptedQuizzes,setAttemptedQuizzes] = useState([]);
 
   useEffect( ()=>{
-    const token = localStorage.getItem('token');
-    const User = jwtDecode(token);
-    // console.log(User);
-    // setLoginUser({
-    //   id:User.id,
-    //     name:User.name,
-    //     email:User.email
-    //   })
-      // console.log(User);
-      // console.log('sending request to getquizzes ',user);
-
+      const token = localStorage.getItem('token');
+      const User = jwtDecode(token);
       const tempuser = {id:User.id,name:User.name,email:User.email};
       console.log('tempuser: ',tempuser);
-    axios.get('http://localhost:8000/getquizzes',tempuser)
-    .then((res)=>{
-      console.log(res.data);
+      axios.post('http://localhost:8000/getquizzes',tempuser)
+      .then((res)=>{
+
+      console.log('response data from getquizzes: ', res.data);
       setCreatedQuizzes(res.data.createdQuizzes);
       setAttemptedQuizzes(res.data.attemptedQuizzes);
     }).catch(err=>console.log(err));
@@ -36,18 +28,18 @@ const Dashboard = ({user,setLoginUser}) => {
   return (
     <div>
         <h1>Created Quizzes</h1>
-        {/* <ul>{
+        {/* { <ul>{
           createdQuizzes.map((quiz,index)=>{
             return <li key={index}><CreatedQuizCard quiz={quiz}/></li>
           })
         }
-        </ul> */}
+        </ul> } */}
         <h1>Attempted Quizzes</h1>
-        {/* <ul>{
+        {/* { <ul>{
           attemptedQuizzes.map((quiz,index)=>{
             return <li key={index}><AttemptedQuizCard quizId={quiz}/></li>
           })
-        }</ul> */}
+        }</ul> } */}
     </div>
   )
 }
