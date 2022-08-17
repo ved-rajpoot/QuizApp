d587c83988ba19e621a2783d0754731580769c86
ved <vedpratap.rajpoot@gmail.com>
2022-08-17 16:49:26 +0530

    added loading screen

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = ({user,setLoginUser}) => {
         navigate('/');    
     }
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-    ">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">Navbar</Link>
         {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
